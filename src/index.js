2d91f1d4f2ed226c7ada1400fc91b74707fd0e66
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-03 20:58:05 -0800

    Update App cto class component.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return <div>Hi there!</div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      errorMessage: ""
+    };
+
+    // getCurrentPosition takes 2 callback functions
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!!
+  render() {
+    if(this.state.errorMessage && !this.state.lat) {
+    return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if(!this.state.errorMessage && this.state.lat) {
+    return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading...</div>;
+    
+    // return (
+    //   <div>
+    //     Latitude: {this.state.lat} <br />
+    //     Error: {this.state.errorMessage}
+    //   </div>
+    //   );
+  }
+}
 
 ReactDOM.render(
   <App />,
