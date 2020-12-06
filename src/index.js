e497581f7248943f750bcb016a2633c1db946fd4
Brittany Lindgren <brittdevelopsit@gmail.com>
2020-12-06 15:50:01 -0800

    Update SeasonDisplay configuration, add styling.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      lat: null,
-      errorMessage: ""
-    };
-
+  state = {
+    lat: null,
+    errorMessage: ""
+  };
+  
+  componentDidMount() {
     // getCurrentPosition takes 2 callback functions
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({ lat: position.coords.latitude });
-      },
+      position =>this.setState({ lat: position.coords.latitude }),
       err => this.setState({ errorMessage: err.message })
     );
   }
@@ -26,17 +23,10 @@ class App extends React.Component {
     }
 
     if(!this.state.errorMessage && this.state.lat) {
-    return <div>Latitude: {this.state.lat}</div>;
+    return <SeasonDisplay lat={this.state.lat}/>
     }
 
     return <div>Loading...</div>;
-    
-    // return (
-    //   <div>
-    //     Latitude: {this.state.lat} <br />
-    //     Error: {this.state.errorMessage}
-    //   </div>
-    //   );
   }
 }
 
